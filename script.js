4d774c97cf0b45dd2179ffc6769a991b6d5ee35a
rgyt <103154533+rg-yt@users.noreply.github.com>
2025-12-08 00:01:07 -0500

    Add gameboard, player, and, game exposing little global values

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("linked");
+const gameboard = (function () {
+  const board = [];
+  return { board };
+})();
+
+const Player = function (name, marker) {
+  return { name, marker };
+};
+
+const game = (function () {
+  let player1 = Player("Tyler", "X");
+  let player2 = Player("Katie", "O");
+  let currentPlayer = player1;
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+  return { getCurrentPlayer, switchPlayer };
+})();
