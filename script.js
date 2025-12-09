5afb4787c91c6ecf6ff98ddc301be27fda0b68cd
rgyt <103154533+rg-yt@users.noreply.github.com>
2025-12-08 19:12:50 -0500

    Expand game and gameboard factories

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const gameboard = (function () {
-  const board = [];
-  return { board };
+  const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const updateBoard = (index, currentPlayer) => {
+    if (typeof board[index] === "number" && index >= 0 && index <= 8) {
+      board[index] = currentPlayer.marker;
+    } else {
+      console.log("Must make a valid move");
+    }
+  };
+
+  const showBoard = () => {
+    console.log(board.slice(0, 3).join(" | "));
+    console.log("---------");
+    console.log(board.slice(3, 6).join(" | "));
+    console.log("---------");
+    console.log(board.slice(6, 9).join(" | "));
+  };
+
+  const valuesAt = (array) => {
+    const newArray = [];
+    array.forEach((element) => {
+      newArray.push(board[element]);
+    });
+    return newArray;
+  };
+  return { board, updateBoard, showBoard, valuesAt };
 })();
 
 const Player = function (name, marker) {
@@ -12,10 +35,40 @@ const game = (function () {
   let player2 = Player("Katie", "O");
   let currentPlayer = player1;
 
+  const getCurrentPlayer = () => currentPlayer;
+
   const switchPlayer = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1;
   };
 
-  const getCurrentPlayer = () => currentPlayer;
-  return { getCurrentPlayer, switchPlayer };
+  const takeTurn = (index) => {
+    this.updateBoard(index, currentPlayer);
+    switchPlayer();
+  };
+
+  const play = () => {};
+
+  const checkWin = (playerSymbol) => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    let hasWon = false;
+    winningCombos.forEach((combo) => {
+      if (gameboard.valuesAt(combo).every((value) => value === playerSymbol)) {
+        hasWon = true;
+      }
+    });
+
+    return hasWon;
+  };
+
+  return { getCurrentPlayer, switchPlayer, takeTurn, checkWin };
 })();
